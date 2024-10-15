f32018b67b67357e255104d82e751fd72b9f2c14
ChristianScarabelli <scachmabol@hotmail.it>
2024-10-15 14:41:59 +0200

    ADD: repo- setup

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* 
+Consegna:
+Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. 
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+*/
+
+// 1. PREPARAZIONE E ACQUISIZIONE DEI DATI
+
+
+
+// 2. ESECUZIONE DELLA LOGICA / ALGORITMO
+
+
+
+// 3. OUTPUT
