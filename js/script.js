162c918ea73db6ae06c34df8ad421ea739bc3249
ChristianScarabelli <scachmabol@hotmail.it>
2024-10-15 15:17:34 +0200

    ADD: stampa dei numeri multipli

--- js/script.js
+++ js/script.js
@@ -5,12 +5,26 @@ ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 */
 
-// 1. PREPARAZIONE E ACQUISIZIONE DEI DATI
+//avvio il loop di 100 iterazioni
+for (let i = 0; i < 100; i++) {
+    // console.log(i)
 
+    // stampo i numeri da 1 a 100
+    const num = i + 1  
+    // console.log(num)  //number
 
+    // stampo i numeri multipli di 3
+    const double3 = num % 3
+    
+    if (double3 === 0) {
+        console.log(num) // num è multiplo di 3
+    }
 
-// 2. ESECUZIONE DELLA LOGICA / ALGORITMO
+    // stampo i numeri multipli di 5
+    const double5 = num % 5
 
+    if (double5 === 0) {
+        console.log(num) // num è multiplo di 5
+    }
+}
 
-
-// 3. OUTPUT
